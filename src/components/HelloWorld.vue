05581c6e8f92f1f5ccc0d92b36afc0e48942ddb2
syed Riyaz <76534599+riyazulahad786@users.noreply.github.com>
2022-05-15 11:21:00 +0530

    Add files via upload

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+   <h1>hi</h1>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+
+    data: () => ({
+    
+         
+  
+    }),
+  }
+</script>
